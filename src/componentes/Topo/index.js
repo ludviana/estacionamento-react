87bf6734b058d760619c967ba052407bf09df1fa
Lucas Damasceno <lucasd.viana1993@gmail.com>
2021-07-17 21:57:49 -0300

    Alteraçoes no formulario de cadastro de estacionamento

--- src/componentes/Topo/index.js
+++ src/componentes/Topo/index.js
@@ -5,42 +5,50 @@ import Navbar from '../Topo2/Navbar';
 
 import './estilo.css';
 
-
-
-
 const Topo = () => {
-  const { authenticated, handleLogout } = useContext(Context);
+	const { authenticated, handleLogout } = useContext(Context);
 
-  
-  
-  
-  return (
-    <header className="topo">
-      <div className="topo-conteudo">
-        <Link className="topo-logo-box" to="/">
-          <img className="topo-logo" src="assets/park.png" alt="logo estacionamento" title="logo estacionamento"/>
-          <p className="topo-nome-empresa">Tem <span>Vaga</span></p>
-        </Link>        
-        <nav className="topo-links">
-          <button className="btn-mobile">Menu</button>
-          <div className="menu">
-            <Link className="topo-link" to="/">Home</Link>
-            <Link className="topo-link" to="/Estacionamentos-Parceiros">Estacionamentos Parceiros</Link>
-            <Link className="topo-link" to="/contato">Contato</Link>
-            {authenticated ? 
-              <span className="topo-link" onClick={handleLogout}>Sair</span>
-              : 
-              <Link className="topo-link" to="/login">Login</Link>
-            }
-          </div>
-        </nav>
-
-      </div>
-      <Navbar/>
-    </header>
-   
-  );
-}
+	return (
+		<header className='topo'>
+			<div className='topo-conteudo'>
+				<Link className='topo-logo-box' to='/'>
+					<img
+						className='topo-logo'
+						src='assets/park.png'
+						alt='logo estacionamento'
+						title='logo estacionamento'
+					/>
+					<p className='topo-nome-empresa'>
+						Tem <span>Vaga</span>
+					</p>
+				</Link>
+				<nav className='topo-links'>
+					<button className='btn-mobile'>Menu</button>
+					<div className='menu'>
+						<Link className='topo-link' to='/'>
+							Home
+						</Link>
+						<Link className='topo-link' to='/Estacionamentos-Parceiros'>
+							Estacionamentos Parceiros
+						</Link>
+						<Link className='topo-link' to='/contato'>
+							Contato
+						</Link>
+						{authenticated ? (
+							<span className='topo-link' onClick={handleLogout}>
+								Sair
+							</span>
+						) : (
+							<Link className='topo-link' to='/login'>
+								Login
+							</Link>
+						)}
+					</div>
+				</nav>
+			</div>
+			<Navbar />
+		</header>
+	);
+};
 
 export default Topo;
-
